823a5f77212d6cd6c9f04ce80c5c750d82513a72
Jeong-Beom <whwjdqja58@naver.com>
2024-07-05 18:09:43 +0900

    Modify bootstrap

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ app_name = 'main'
 urlpatterns = [
     # base_views.py
     path('', base_views.index, name='index'),
+    path('portfolio_details/', base_views.portfolio_details, name='portfolio_details'),
+    path('service_details/', base_views.service_details, name='service_details'),
+    path('starter_page/', base_views.starter_page, name='starter_page'),
 ]
--- main/views/base_views.py
+++ main/views/base_views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def portfolio_details(request):
+    return render(request, 'main/portfolio-details.html')
+
+def service_details(request):
+    return render(request, 'main/service-details.html')
+
+def starter_page(request):
+    return render(request, 'main/starter-page.html')
