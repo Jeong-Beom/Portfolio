26ee551fff8bf6885617e40f0b9de9c3f4bd758b
Jeong-Beom <whwjdqja58@naver.com>
2024-07-10 18:00:53 +0900

    Add files

---
+++ enter_prod.sh
@@ -0,0 +1,9 @@
+# load .bashrc
+. ~/.bashrc
+
+# activate virtual python environment
+pyenv activate venv_portfolio
+
+# add server execute file path
+export DJANGO_SETTINGS_MODULE=config.settings.prod
+
