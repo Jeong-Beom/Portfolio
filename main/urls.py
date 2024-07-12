3732994800d438afaffcddb4d824b9d9b91e83f5
Jeong-Beom <whwjdqja58@naver.com>
2024-07-12 18:52:55 +0900

    Create service detail pages

--- main/urls.py
+++ main/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('portfolio_details/pp1', portfolio_details_views.portfolio_details_pp1, name='portfolio_details_pp1'),
     
     # service_details_views_py
-    path('service_details/sample', service_details_views.service_details_sample, name='service_details_sample'),
+    path('service_details/ltmetal', service_details_views.service_details_ltmetal, name='service_details_ltmetal'),
+    path('service_details/samsungelectronics', service_details_views.service_details_samsungelectronics, name='service_details_samsungelectronics'),
+    path('service_details/golfzon', service_details_views.service_details_golfzon, name='service_details_golfzon'),
 ]
--- main/views/service_details_views.py
+++ main/views/service_details_views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
-def service_details_sample(request):
-    return render(request, 'main/service-details-sample.html')
+def service_details_ltmetal(request):
+    return render(request, 'main/service-details-ltmetal.html')
+
+def service_details_samsungelectronics(request):
+    return render(request, 'main/service-details-samsungelectronics.html')
+    
+def service_details_golfzon(request):
+    return render(request, 'main/service-details-golfzon.html')
